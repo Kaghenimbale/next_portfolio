84464958152cd378ba59f73d7ad12aecd8a6bb11
Kaghenimbale <kaghenimbale@gmail.com>
2024-05-20 15:46:12 +0200

    Add modification to my aboutme component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a1c924222e30a89"; }
+/******/ 		__webpack_require__.h = function() { return "5157dabf5c04bea2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
